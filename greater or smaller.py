6a13b9c4bee3d5d5f628022cead19d59317650f0
samir84thakkar-blip <samir84thakkar@gmail.com>
2026-08-13 11:45:49 +0200

    Add files via upload

---
+++ greater or smaller.py
@@ -0,0 +1,6 @@
+a=int(input("Enter any number"))
+c=int(input("Enter any number"))
+if a<=c:
+    print("a is smaller than or equal to c")
+else:
+    print("a is bigger than c")
---
+++ profit or loss.py
@@ -0,0 +1,7 @@
+cp=int(input("Enter cost price"))
+sp= int(input("Enter selling price"))
+if sp<cp:
+    print("It is a loss")
+else:
+    print("You are in profit")
+    
