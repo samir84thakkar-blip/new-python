ab82d416a017b9f597ce02192692eab43753d729
samir84thakkar-blip <samir84thakkar@gmail.com>
2026-08-28 16:33:12 +0200

    Add files via upload

---
+++ power.py
@@ -0,0 +1,4 @@
+s=int(input("Enter any number to find its power:"))
+p=int(input("Enter the power to which you want to raise the number:"))
+result=s**p
+print("The result of",s,"raised to the power of",p,"is:",result)
